516b1a2ef01dd0952e4aba07a2ee1e1f4fc2bd0e
Juliano Mohr <1110178+juliaaano@users.noreply.github.com>
2025-10-10 10:33:31 +0100

    Add Istio sidecar label to travel-control VM

--- lab-3/add-envoy-to-travel-control.sh
+++ lab-3/add-envoy-to-travel-control.sh
@@ -7,6 +7,7 @@ VM_POOL_NAME=$(oc get VirtualMachine -o jsonpath='{.items[0].metadata.name}')
 echo $VM_POOL_NAME
 
 oc patch VirtualMachine/$VM_POOL_NAME --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-control
+oc patch VirtualMachine/$VM_POOL_NAME --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-control
 oc delete pods -l vm.kubevirt.io/name=$VM_POOL_NAME -n travel-control
 
 echo
