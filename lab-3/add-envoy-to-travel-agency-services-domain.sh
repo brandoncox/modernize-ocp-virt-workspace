b8a5c1ba1c3a6b6264ca034e43292a21cb0d5497
Juliano Mohr <1110178+juliaaano@users.noreply.github.com>
2025-10-10 09:57:45 +0100

    Fix VM configs for sm3

--- lab-3/add-envoy-to-travel-agency-services-domain.sh
+++ lab-3/add-envoy-to-travel-agency-services-domain.sh
@@ -11,30 +11,37 @@ echo
 
 
 oc patch VirtualMachine/cars-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/cars-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=cars-vm -n travel-agency
 
 
 oc patch VirtualMachine/discounts-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/discounts-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=discounts-vm -n travel-agency
 
 
 oc patch VirtualMachine/flights-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/flights-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=flights-vm -n travel-agency
 
 
 oc patch VirtualMachine/hotels-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/hotels-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=hotels-vm -n travel-agency
 
 
 oc patch VirtualMachine/insurances-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/insurances-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=insurances-vm -n travel-agency
 
 
 oc patch VirtualMachine/mysqldb-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/mysqldb-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=mysqldb-vm -n travel-agency
 
 
 oc patch VirtualMachine/travels-vm --type merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
+oc patch VirtualMachine/travels-vm --type merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-agency
 oc delete pods -l vm.kubevirt.io/name=travels-vm -n travel-agency
 
 echo
--- lab-3/add-envoy-to-travel-portal-domain.sh
+++ lab-3/add-envoy-to-travel-portal-domain.sh
@@ -6,9 +6,9 @@ echo
 echo "Add Deployments in the mesh by injecting Service Mesh sidecars to components"
 echo "---------------------------------------------------------------------------------"
 echo
-oc patch deployment/travels --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-portal
-oc patch deployment/viaggi --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-portal
-oc patch deployment/voyages --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-portal
+oc patch deployment/travels --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-portal
+oc patch deployment/viaggi --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-portal
+oc patch deployment/voyages --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-portal
 
 echo
 echo
